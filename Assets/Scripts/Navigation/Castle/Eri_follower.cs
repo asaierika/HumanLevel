1e9948eaafd3c0ea466dfc4058c01ce17fb477f8
Jiang Zhimeng <anyaasai@gmail.com>
2023-01-18 15:19:17 +0800

    Refactor Choice System
    Change Choice System to make use of EventManager instead of
    ScriptableObject GameEvent.

--- Assets/Scripts/Navigation/Castle/Eri_follower.cs
+++ Assets/Scripts/Navigation/Castle/Eri_follower.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class Eri_follower : Follower
 {
     public Conversation convo;
-    public Choice choice1, choice2;
+    public string choice1, choice2;
     public GameEvent chaseEnded;
+    public Vector2 hallPosition;
 
     protected override void Found()
     {
         // Eri is dynamically generated at each room. Both managers cannot be assigned before hand.
-        GameObject.FindObjectOfType<DialogueManager>().StartConversation(convo);
-        GameObject.FindObjectOfType<ChoiceManager>().StartChoice(choice1, choice2);
+        DialogueManager.instance.StartConversation(convo);
+        EventManager.StartListening(EventManager.Event.CHOICE_ONE, Choice1);
+        EventManager.StartListening(EventManager.Event.CHOICE_TWO, Choice2);
+        ChoiceManager.instance.StartChoice(choice1, choice2);
         chaseEnded.TriggerEvent();
+    }
+
+    public void Choice1(object o = null)
+    {
+        PlayerSpawner.AssignSpawnPoint(hallPosition);
+        FollowingManager.instance.StartFollowing();
+        SceneTransition.SceneTransit("Hall");
+    }
+
+    public void Choice2(object o = null)
+    {
+        PlayerSpawner.AssignSpawnPoint(hallPosition);
+        // Remove the singleton
+        Destroy(FollowingManager.instance);
+        SceneTransition.SceneTransit("Hall_Before_Chase");
     }
 }
